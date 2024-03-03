22ca2dd741da98066b24684c4482aa1c252fbb57
Rodriginsky <rodrigoestrategia2000@gmail.com>
2024-03-03 16:57:42 -0600

    Network solved

--- commands.sh
+++ commands.sh
@@ -11,10 +11,13 @@ docker run --name python python
 docker compose up --force-recreate --build -f ./docker-compose-db.yml
 docker compose up --force-recreate --build -f ./docker-compose-python.yml
 # ?
-docker compose -f docker-compose.yml  up --force-recreate --build 
+docker compose -f ./docker-compose.yml  up --force-recreate --build 
+docker compose -f ./docker-compose.yml  down
 docker compose -f ./docker-compose-db.yml  up --force-recreate --build 
+docker compose -f ./docker-compose-db.yml  down
 
 sudo docker start -i proyectofinal-python-app-1
 sudo docker exec -it proyectofinal-db-1 /bin/sh
+sudo docker exec -it proyectofinal-python-app-1 /bin/sh
 
 sudo service mysql stop
